5e395e29159d347ce4b182bf4641f051a5d60cf7
minchit <triet@gmail.com>
2025-09-19 15:41:52 +0700

    Initial commit

---
+++ src/main/java/com/group02/ev_maintenancesystem/EvMaintenanceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.group02.ev_maintenancesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvMaintenanceSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvMaintenanceSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group02/ev_maintenancesystem/EvMaintenanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group02.ev_maintenancesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvMaintenanceSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
