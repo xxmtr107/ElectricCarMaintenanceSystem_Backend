a382b8c07ffd3ea9447689883f40ccf28211f620
minchit <triet@gmail.com>
2025-10-02 17:22:05 +0700

    config Gender

--- src/main/java/com/group02/ev_maintenancesystem/configuration/SecurityConfig.java
+++ src/main/java/com/group02/ev_maintenancesystem/configuration/SecurityConfig.java
@@ -18,7 +18,9 @@ import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
