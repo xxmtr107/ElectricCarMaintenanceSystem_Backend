63db2c0d794e08467e125f7ac2c7660d75b31fca
minchit <trietpm.ce190589@gmail.com>
2025-11-06 01:36:32 +0700

    ** Add & Update -  Add 'WAITING_FOR_APPROVAL' status and `AppointmentServiceItemDetail`, manage state (CHECK/REPLACE) for appointment - Add new endpoints for Technician to upgrade service (`/items/upgrade`) and Staff to approve (`/items/approve`) - Update `MaintenanceRecordServiceImpl` to auto-deduct these 'included' parts from inventory upon `COMPLETED` status. - Staff/Technicians are now restricted to viewing Appointments/Records within their own Service Center. - Add business hour validation (7AM-6PM) to appointment creation logic . - Add new ErrorCodes for new validation rules. - Update data.sql

---
+++ src/main/java/com/group02/ev_maintenancesystem/entity/AppointmentServiceItemDetail.java
@@ -0,0 +1,4 @@
+package com.group02.ev_maintenancesystem.entity;
+
+public class AppointmentServiceItemDetail {
+}
---
+++ src/main/java/com/group02/ev_maintenancesystem/repository/AppointmentServiceItemDetailRepository.java
@@ -0,0 +1,4 @@
+package com.group02.ev_maintenancesystem.repository;
+
+public interface AppointmentServiceItemDetailRepository {
+}
