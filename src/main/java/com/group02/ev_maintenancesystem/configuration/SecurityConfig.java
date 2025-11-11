8a041e0d41f9e2cd81bf175b637a0facc2d4d7ba
ThuThao <thaontse184164@fpt.edu.vn>
2025-11-11 22:46:28 +0700

    Fix link ping

--- src/main/java/com/group02/ev_maintenancesystem/configuration/SecurityConfig.java
+++ src/main/java/com/group02/ev_maintenancesystem/configuration/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
                                 "/auth/**",                // Login, Refresh, Logout, Introspect
                                 "/api/ws/**", //Cho phép WebSocket
                                 "/ws/**",
-                                "/vnpay/**"
+                                "/vnpay/**", "/api/ping"
 
                         ).permitAll()
                         // Allow GET all vehicle models and service centers
