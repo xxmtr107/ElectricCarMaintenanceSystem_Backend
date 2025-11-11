2f7320641e20fe43336d3fd6c8c81b74275d25a5
minchit <trietpm.ce190589@gmail.com>
2025-11-11 14:18:53 +0700

    Fixed error name vnPay -> vnpay

--- src/main/java/com/group02/ev_maintenancesystem/controller/VNPayController.java
+++ src/main/java/com/group02/ev_maintenancesystem/controller/VNPayController.java
@@ -19,7 +19,7 @@
     import java.util.stream.Collectors;
 
     @RestController
-    @RequestMapping("/vnPay")
+    @RequestMapping("/vnpay")
     public class VNPayController {
 
         @Autowired
